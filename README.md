df35b0e20b8b94c035a812c0b1d72e735ab1d4cb
tinyshellzz <tinyshellzz@gmail.com>
2025-06-04 14:04:03 +0800

    更新gradle

---
+++ README.md
@@ -0,0 +1,18 @@
+### 使用方式  
+回档命令
+```
+/NeotccInv rollback <玩家> <秒>
+```
+重新加载插件配置文件
+```
+/NeotccInv reload
+```
+查看别人的背包
+```
+/NeotccInv see <玩家>
+```
+查看别人的末影箱
+```
+/NeotccInv ender <玩家>
+```
+
---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import java.text.SimpleDateFormat
+version = "1.1.0"
+
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-public/")
+    maven("https://repo.extendedclip.com/releases/")
+    maven("https://repo.dmulloy2.net/repository/public/")
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("dev.folia:folia-api:1.20.1-R0.1-SNAPSHOT")
+    implementation("com.google.code.gson:gson:2.11.0")
+}
+
+group = "com.tinyshellzz"
+var pluginVersion: String = version.toString() + "-" + SimpleDateFormat("yyyyMMdd").format(System.currentTimeMillis())
+java.sourceCompatibility = JavaVersion.VERSION_21
+java.targetCompatibility = JavaVersion.VERSION_21
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks {
+    val props = HashMap<String, String>()
+    props["version"] = pluginVersion
+    processResources {
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+        filesMatching("config.yml") {
+            expand(props)
+        }
+    }
+    compileJava {
+        dependsOn(clean)
+        options.encoding = "UTF-8"
+    }
+}
